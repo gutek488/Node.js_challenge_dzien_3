0305cd0f07437be437beec2d95792f23bec99531
Gustaw <gutek488@gmail.com>
2019-01-25 15:48:03 +0100

    Zadanie 1 i 2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,29 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
+const crypto = require('crypto');
+let passwords = ['??TegoHasła','CodersLab','Node.js Szyfruje Pliki','Zaźółć Gęślą Jaźń','Moje Haslo 1@3!','111#$((@)n','Dzisiaj Szyfruje 83'];
+let algorithms = ['sha256', 'sha512','md5','rmd160'];
 
-//Twój kod
+//function decodeText(encodedText, password, algorithm){
+//     const decipher = crypto.createDecipher(algorithm, password);
+//     let decrypted = decipher.update(encodedText, 'hex', 'utf8');
+//     decrypted += decipher.final('utf8');
+//     return decrypted;
+// }
+//
+//
+// for (let i = 0; i < passwords.length ; i++) {
+//      for (let j = 0; j < algorithms.length ; j++) {
+//          console.log(decodeText(MY_PWD_HASH, passwords[i], algorithms[i]));
+//      }
+// }
+
+
+passwords.forEach(password => {
+    algorithms.forEach(algorithm => {
+        const hash = crypto.createHmac(algorithm, password)
+            .digest('hex');
+        if(hash === MY_PWD_HASH) {
+            console.log(password, algorithm, hash);
+        }
+    })
+});
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,21 @@
-//Twój kod
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+
+fs.readFile(`${process.argv[2]}`,"utf8", (err,data) => {
+    if (err === null) {
+        let hash = crypto.createHmac('sha256', data).digest("hex");
+        console.log(hash);
+        return hash;
+
+
+    } else {
+        console.log("Błąd przy odczycie pliku", err)
+    }
+
+
+
+});
+
+
